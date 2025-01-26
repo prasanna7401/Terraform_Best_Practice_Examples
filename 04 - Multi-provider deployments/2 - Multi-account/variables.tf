d78771085eae70c1564c81a21c7d3c3f01c60bec
Prasanna Aravindan <prasanna7401@gmail.com>
2025-01-26 04:49:39 -0800

    removed sensitive values (#16)
    * completed multi-region & multi-account setup
    
    * removed sensitive values

--- 04 - Multi-provider deployments/2 - Multi-account/variables.tf
+++ 04 - Multi-provider deployments/2 - Multi-account/variables.tf
@@ -1,5 +1,5 @@
 variable "dev_role_arn" {
   description = "The ARN of the role to assume in the dev account"
   type = string
-  default = "arn:aws:iam::218323737322:role/OrganizationAccountAccessRole"
+  default = "arn:aws:iam::<YOUR_ACCOUNT_ID>:role/<YOUR_CROSS_ACCOUNT_ROLE>"
 }
